4805be2428568729d8f72f9ea04d5bb9e4b774eb
Reviza2308 <112670494+Reviza2308@users.noreply.github.com>
2022-10-21 08:11:26 +0700

    Add files via upload

---
+++ JS9/src/js9/latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package js9;
+
+/**
+ *
+ * @author PC i8
+ */
+public class latihan1 {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+        
+        test(i);
+        System.out.println(i);
+    }
+    public static void test(int j){
+        j=33;
+    }
+}
